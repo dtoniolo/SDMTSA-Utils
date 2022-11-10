1a3f45d7c74c2c7ca6a45e4cf02bc68e29b666a0
Davide Toniolo <d.toniolo2@campus.unimib.it>
2022-11-10 17:40:37 +0100

    Makes the whole package pip installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(name='sdmtsa_utils',
+      version='v0.0.1',
+      url='https://github.com/dtoniolo/SDMTSA-Utils',
+      author='Davide Toniolo',
+      author_email='d.toniolo2@campus.unimib.it',
+      licence='MIT',
+      packages=find_packages())
