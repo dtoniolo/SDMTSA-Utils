699e77c6352329c9be85dfc17dd368516535cbff
Davide Toniolo <d.toniolo2@campus.unimib.it>
2022-11-11 08:22:48 +0100

    Improvements to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 
-setup(name='plotly_utils',
+setup(name='plotly-utils',
       version='v0.0.2',
       description='Utilities for statistical plots with Plotly',
       url='https://github.com/dtoniolo/SDMTSA-Utils',
       author='Davide Toniolo',
       author_email='d.toniolo2@campus.unimib.it',
       licence='MIT',
-      packages=find_packages(include=['plotly_utils', 'plotly_utils.*']))
+      packages=['plotly_utils'])
